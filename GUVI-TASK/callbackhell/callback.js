4afde83e78d256b5f2c5cd072e6d362a357ae7fb
pranil5134 <43214416+pranil5134@users.noreply.github.com>
2021-04-04 10:18:55 +0530

    added

---
+++ GUVI-TASK/callbackhell/callback.js
@@ -0,0 +1,48 @@
+let maindiv=document.createElement('div')
+maindiv.setAttribute('class','d-flex justify-content-center')
+let h1=document.createElement('h3')
+
+maindiv.appendChild(h1)
+document.body.append(maindiv)
+
+
+calculator(4,5,sum,div,mul,sub)
+var count=10
+setTimeout(() => {
+    h1.innerText=count
+    
+    setTimeout(() => {
+        h1.innerText=count-1
+      setTimeout(() => {
+        h1.innerText=count-2 
+          setTimeout(() => {
+            h1.innerText=count-3 
+             setTimeout(() => {
+                h1.innerText=count-4
+                setTimeout(() => {
+                    h1.innerText=count-5
+                    setTimeout(() => {
+                        h1.innerText=count-6
+                      setTimeout(() => {
+                        h1.innerText=count-7
+                        setTimeout(() => {
+                            h1.innerText=count-8
+                            setTimeout(() => {
+                                h1.innerText=count-9
+                                setTimeout(() => {
+                                    h1.innerText=count-10 
+                                    setTimeout(() => {
+                                        h1.innerText="Happy Independence Day"
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000); 
+                        }, 1000); 
+                      }, 1000);  
+                    }, 1000);
+                }, 1000);  
+             }, 1000); 
+          }, 1000);
+      }, 1000);
+    }, 1000);
+}, 1000);
+
